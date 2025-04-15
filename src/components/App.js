822570af2d45fb06317e8d1770479a7e83cce7ee
hassan-abdi269 <honze2030@gamail.com>
2025-04-15 11:58:32 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <h1>My portfolio</h1>
     </nav>
   );
 }
@@ -11,20 +11,25 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>Welcome to my portfolio</h1>
     </div>
   );
 }
-
-{/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
